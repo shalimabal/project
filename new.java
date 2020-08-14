087ded77c655e4b49eb1fd8620eeb404f3c18fb2
shalidevops <shalidevops@gmail.com>
2020-08-14 09:50:58 +0000

    third commit

---
+++ new.java
@@ -0,0 +1,54 @@
+ghghghghgh..hlrhello:wq
+:wq
+
+::wq:
+:x
+
+
+fgkfkgfkgfkgkfgkfgkfkgkfgkfgk
+kfgkfgkfgkfkgfkggfkgfg
+:wq
+
+:wq
+:wq
+!q
+
+:wq
+
+:wq
+:wq
+fghfghfhgfhgfhgfhgfgh
+jjfgfgfg
+jjfgfgfgfg
+fgfgfgfggfggggg
+kfkgfkgfkgkfgkfgkfgk
+jfg
+
+jfg
+kkfgfg
+kjfjfjfg
+kkfgfgfg
+fgjfgjfjgfkfkgfg
+fgfg
+h
+
+h
+h
+
+h
+h
+
+::wq
+:wq  
+
+
+ :wq
+
+ h
+ h
+ :
+
+ :wq
+ :wq
+ :q!
+
